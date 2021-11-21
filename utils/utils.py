e3fce02f3df43166f05dae21eb4b70bb2fd45750
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2021-11-21 14:31:06 -0600

    latest changes

---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import math
+
+def create_matrix(X,fractionObserved=0.9):
+    '''
+    Recieves a matrix X and returns a matrix with the same dimensions and
+    with a fraction of the observed records.
+    '''
+    Omega = np.array(np.random.rand(X.shape[0],X.shape[1])< fractionObserved)
+    Xobs = Omega*X
+    return Xobs, Omega
+
+
+
